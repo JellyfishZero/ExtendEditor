40c5fca2165ac6d7d3ba7a7e3aaac6c1ec60b45f
xander_yuan <xander_yuan@softstar.com.tw>
2024-07-03 16:07:26 +0800

    feat: msg box

--- Plugins/SuperManager/Source/SuperManager/Private/AssetActions/QuickAssetAction.cpp
+++ Plugins/SuperManager/Source/SuperManager/Private/AssetActions/QuickAssetAction.cpp
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "AssetActions/QuickAssetAction.h"
@@ -11,7 +11,9 @@ void UQuickAssetAction::DuplicateAssets(int32 NumOfDuplicates)
 {
 	if (NumOfDuplicates <= 0)
 	{
-		Print(TEXT("Please enter a valid number"), FColor::Red);
+		/*Print(TEXT("Please enter a valid number"), FColor::Red);*/
+		FText MsgTitle = FText::FromString(TEXT("警告"));
+		EAppReturnType::Type UserInputResult = FMessageDialog::Open(EAppMsgType::Ok, FText::FromString(TEXT("欸！給我打大於0的數值")), MsgTitle);
 		return;
 	}
 
@@ -22,7 +24,7 @@ void UQuickAssetAction::DuplicateAssets(int32 NumOfDuplicates)
 	{
 		for (int32 i = 0; i < NumOfDuplicates; ++i)
 		{
-			const FString SourceAssetPath = SelectedAssetData.ObjectPath.ToString();
+			const FString SourceAssetPath = SelectedAssetData.GetSoftObjectPath().ToString();
 			const FString NewDuplicatedAssetName = SelectedAssetData.AssetName.ToString() + TEXT("_") + FString::FromInt(i+1);
 			const FString NewPathName = FPaths::Combine(SelectedAssetData.PackagePath.ToString(), NewDuplicatedAssetName);
 
--- Plugins/SuperManager/Source/SuperManager/Public/DebugHeader.h
+++ Plugins/SuperManager/Source/SuperManager/Public/DebugHeader.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "Misc/MessageDialog.h"
+
 void Print(const FString& Message, const FColor& Color)
 {
 	if (GEngine)
@@ -11,4 +13,17 @@ void Print(const FString& Message, const FColor& Color)
 void PrintLog(const FString& Message)
 {
 	UE_LOG(LogTemp, Warning, TEXT("%s"), *Message);
+}
+
+EAppReturnType::Type ShowMsgDialog(EAppMsgType::Type MsgType, const FString& Message, bool bShowMsgAsWarning = true)
+{
+	if (bShowMsgAsWarning)
+	{
+		FText MsgTitle = FText::FromString(TEXT("Äµ§i"));
+		return FMessageDialog::Open(MsgType, FText::FromString(Message), MsgTitle);
+	}
+	else
+	{
+		return FMessageDialog::Open(MsgType, FText::FromString(Message));
+	}
 }
